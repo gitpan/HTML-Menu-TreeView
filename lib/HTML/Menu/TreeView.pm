27200211c692cb85b425e0086117f9c037cc5fb9
Dirk Lindner <lze@cpan.org>
2008-03-22 08:14:05 +0000

    Import of LZE/HTML-Menu-TreeView-0.88 from CPAN.
    gitpan-cpan-distribution: HTML-Menu-TreeView
    gitpan-cpan-version:      0.88
    gitpan-cpan-path:         LZE/HTML-Menu-TreeView-0.88.tar.gz
    gitpan-cpan-author:       LZE
    gitpan-cpan-maturity:     released

--- lib/HTML/Menu/TreeView.pm
+++ lib/HTML/Menu/TreeView.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 require Exporter;
 use vars qw($DefaultClass %EXPORT_TAGS @EXPORT_OK @ISA %anker @TreeView %openArrays @caption $columns $clasic $ffirst $sort $border $orderby $size $style $orderbyColumn $prefix);
-$HTML::Menu::TreeView::VERSION = '0.87';
+$HTML::Menu::TreeView::VERSION = '0.88';
 @ISA                           = qw(Exporter);
 @HTML::Menu::TreeView::EXPORT_OK =
   qw(border Tree css columns jscript setStyle setDocumentRoot getDocumentRoot setSize setClasic clasic preload help folderFirst size style Style documentRoot loadTree saveTree  %anker sortTree orderBy orderByColumn prefix setModern border);
